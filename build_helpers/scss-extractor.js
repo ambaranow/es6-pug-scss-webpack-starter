3a147429bb8b5e38f0530cbca1a7ca5fe928c845
ambaranow <a.baranov@notamedia.ru>
2017-09-19 22:20:49 +0300

    add components, create includes array

--- build_helpers/scss-extractor.js
+++ build_helpers/scss-extractor.js
@@ -1,5 +1,8 @@
 const fs = require('fs')
 const path = require('path')
+const load = require('pug-load')
+var lex = require('pug-lexer')
+var parse = require('pug-parser')
 
 const walk = function (directoryName, tmplExt) {
   const pages = []
@@ -21,6 +24,23 @@ const walk = function (directoryName, tmplExt) {
 }
 
 module.exports = function extractScss(pagesDir, tmplExt, stylePath) {
-  const pagesArr = walk(pagesDir, tmplExt)
+  const pagesArr = walk(pagesDir, tmplExt) // root pages templates array
+  let i=pagesArr.length
+  while (i--) {
+    let scssArr = []
+    load.file(pagesArr[i], {
+      lex: lex,
+      parse: parse,
+      resolve: function (filename, source, options) {
+        // console.log('"' + filename + '" file requested from "' + source + '".')
+        // console.log('......')
+        scssArr.push(path.resolve(source, filename)) // full path for components(includes)
+        return load.resolve(filename, source, options)
+      }
+    })
+    console.log('============================')
+    console.log(scssArr)
+    // console.log('----------------------------')
+  }
   return pagesArr
 }
